d8c72f55bbcacb4d4346b2393fdc0cb2f0681b25
Jackson Ho <mintri1199@gmail.com>
2020-07-04 04:55:50 -0400

    [Modified] finished project 6 challenges

--- 100_SwiftUI/ChooseTheFlag.swift
+++ 100_SwiftUI/ChooseTheFlag.swift
@@ -9,18 +9,16 @@
 import SwiftUI
 
 struct FlagImage: View {
-  
   let imageString: String
   
   var body: some View {
     Image(imageString)
-    .renderingMode(.original)
-    .clipShape(Capsule())
-    .overlay(Capsule().stroke(Color.black, lineWidth: 1))
-    .shadow(color: .black, radius: 2)
+      .renderingMode(.original)
+      .clipShape(Capsule())
+      .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+      .shadow(color: .black, radius: 2)
   }
 }
-
 
 struct ChooseTheFlag: View {
   @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
@@ -29,7 +27,12 @@ struct ChooseTheFlag: View {
   @State private var scoreTitle = ""
   @State private var selectedAnswer = 0
   @State private var userScore = 0
-  @State private var correct = false
+  @State private var opacityAmount = 1.0
+  @State private var cellSpinArray = [0.0, 0.0, 0.0]
+  @State private var cellOpacityBool = [true, true, true]
+  @State private var incorrectOverlay = [false, false, false]
+  
+  @State private var correct = true
   var body: some View {
     ZStack {
       LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
@@ -52,41 +55,76 @@ struct ChooseTheFlag: View {
             self.flagTapped(number)
           }) {
             FlagImage(imageString: self.countries[number])
-          } 
+              .rotation3DEffect(.degrees(cellSpinArray[number]), axis: (x: 0.0, y: 1.0, z: 0.0), anchor: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+              .opacity(cellOpacityBool[number] ? 1.0 : 0.25)
+              .overlay(Color.red.opacity(incorrectOverlay[number] ? 0.75 : 0).clipShape(Capsule()))
+          }
         }
         
         Text("Score: \(userScore)")
-        .foregroundColor(.white)
-        .font(.largeTitle)
-        .fontWeight(.black)
+          .foregroundColor(.white)
+          .font(.largeTitle)
+          .fontWeight(.black)
         Spacer()
       }
-    }.alert(isPresented: self.$showingScore) {
-      Alert(title: Text(self.scoreTitle), message: Text("That was the flag of \(countries[selectedAnswer])"), dismissButton: .default(Text("Continue")) {
-        self.askQuestion()
-        })
     }
+    .alert(isPresented: self.$showingScore) {
+      Alert(title: Text(self.scoreTitle),
+            message: Text("That was the flag of \(countries[selectedAnswer])"),
+            dismissButton: .default(Text("Continue")) {
+              self.askQuestion()
+            })
+    }
+    
+    
   }
-  
   func flagTapped(_ number: Int) {
+    let alertDelay = 1.5
     if number == correctAnswer {
       scoreTitle = "Correct"
       userScore += 1
       correct = true
+      
+      withAnimation(.default) {
+        self.cellSpinArray[number] += 360
+        
+        for i in 0..<3 {
+          if i != number {
+            self.cellOpacityBool[i] = false
+          }
+        }
+      }
+      
     } else {
       scoreTitle = "Wrong"
       userScore -= 1
       correct = false
+      
+      withAnimation(.default) {
+        self.incorrectOverlay[number] = true
+        
+        for i in 0..<3 {
+          if i != number {
+            self.cellOpacityBool[i] = false
+          }
+        }
+      }
     }
-    showingScore = true
     
+    DispatchQueue.main.asyncAfter(deadline: .now() + alertDelay) {
+      self.showingScore = true
+    }
   }
   
   func askQuestion() {
     countries.shuffle()
     correctAnswer = Int.random(in: 0...2)
+    cellSpinArray = [0.0, 0.0, 0.0]
+    cellOpacityBool = [true, true , true]
+    incorrectOverlay = [false, false, false]
   }
 }
+
 
 struct ChooseTheFlag_Preview: PreviewProvider {
   static var previews: some View {
